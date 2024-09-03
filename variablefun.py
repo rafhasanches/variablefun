4683c720f0154a1319ea4386785579b0f7b240a2
rafha <rsanchesmontenegr@mail.valenciacollege.edu>
2024-09-02 22:03:38 -0400

    First Commit

---
+++ variablefun.py
@@ -0,0 +1,20 @@
+x1 = 5
+x2 = 6
+x3 = 8
+x4 = 3
+x5 = 4300000
+x6 = 23090
+x7 = 7633
+
+answer1 = x1 * x2
+answer2 = x3 / x4
+answer3 = x3 // x4
+answer4 = x5 + x6
+answer5 = x5 - x7
+print("The answer to your query is", answer1)
+print("The answer to your query is", answer2)
+print("The answer to your query is", int(answer2))
+print("The answer to your query is", answer3)
+print("The answer to your query is", answer4)
+print("The answer to your query is", answer5)
+print("Rafhael Sanches Montenegro da Cunha")
